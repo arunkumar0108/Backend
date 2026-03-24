195957890ac3ac8e364ccada63a5ae9a5d3e453e
arunkumar0108 <kmarunkumarar0108@gmail.com>
2026-03-24 17:06:39 +0530

    Added EventHandler

---
+++ Backend/EventHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend
+{
+
+    // Instead of custom delegates, use built-in pattern:
+    // EventHandler
+    // EventHandler<TEventArgs>
+    // Why this pattern?
+    // 1. Standard across.NET
+    // 2. Easy to extend
+    // 3. Clean architecture
+
+    class OrderEventArgs : EventArgs
+    {
+        public string? OrderId { get; set; }
+    }
+
+    // Publisher
+    class OrderService
+    {
+        public event EventHandler<OrderEventArgs>? OnOrderPlaced;
+
+        public void PlaceOrder(string id)
+        {
+            Console.WriteLine("Order created");
+
+            OnOrderPlaced?.Invoke(this, new OrderEventArgs { OrderId = id });
+        }
+    }
+
+    // Subscribers
+    class EmailService
+    {
+        public void SendEmail(object? sender, OrderEventArgs? e)
+        {
+            Console.WriteLine($"Email sent for {e?.OrderId}");
+        }
+    }
+    class SmsService
+    {
+        public void SendSms(object? sender, OrderEventArgs? e)
+        {
+            Console.WriteLine($"SMS sent for {e?.OrderId}");
+        }
+    }
+
+    // Additionaly add logging, inventory update, etc.
+}
+
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,101 +1,113 @@
 ﻿using Backend;
 
-public class Button
+namespace Backend
 {
-    // Declare an event using a delegate type
-    public event EventHandler? OnClick;
-
-    public void Click()
+    public class Button
     {
-        Console.WriteLine("Button clicked!");
-        OnClick?.Invoke(this, EventArgs.Empty); // Raise the event
-    }
-}
+        // Declare an event using a delegate type
+        public event EventHandler? OnClick;
 
-
-public class Program
-{
-
-    public delegate int MyDelegate(int a, int b);
-    public delegate bool Filter(int x);
-    public delegate void Callback();
-
-    // MyDelegate method
-    public int Add(int x, int y)
-    { 
-        return x + y;
-    }
-    public int Sub(int x, int y)
-    {
-       return (x - y);
-
-    }
-
-    // Normal method to print numbers based on a filter
-    static void PrintNumbers(List<int> list, Filter filter)
-    {
-        foreach (var num in list)
+        public void Click()
         {
-            if (filter(num))
-                Console.WriteLine(num);
+            Console.WriteLine("Button clicked!");
+            OnClick?.Invoke(this, EventArgs.Empty); // Raise the event
         }
-    }    
-
-    // Callback function in Delegate
-    static void DoWork(Callback callback)
-    {
-        Console.WriteLine("Work started...");
-        callback();
-    }
-    static void Main()
-    {
-
-        DoWork(() => Console.WriteLine("Work completed!"));
-
-        List<int> numbers = new List<int>();
-        PrintNumbers(numbers, x => x > 5);
-        PrintNumbers(numbers, x => x % 2 == 0);
-
-        Console.WriteLine(numbers);
-
-        Program program = new Program();
-
-        MyDelegate del = program.Add;   // store method
-        MyDelegate del1 = program.Sub;   // store method
-        int result = del(5, 3); // call method
-        int result1 = del1(5, 3); // call method
-
-        Console.WriteLine(result);
-        Console.WriteLine(result1);
-
-        // Hashcode - unique identifier for an object
-        string name = "Kumar";
-        int hashCode = name.GetHashCode(); // → oru unique number kedaikum
-        Console.WriteLine(hashCode); // e.g: 1823746
-
-
-        Calculator calc = new Calculator();
-        int calculateResult = calc.Add(10, 5);
-        Console.WriteLine(calculateResult);
-
-        // Linq
-        List<int> numerics = new List<int>() { 1, 2, 3, 4, 5 };
-
-        var resultLinq = numbers.Where(x => x > 3).ToList();
-        Console.WriteLine(string.Join(", ", resultLinq));
-
-
-        // Event handling
-        Button btn = new Button();
-
-        // Subscribe to the event
-        btn.OnClick += HandleClick;
-
-        btn.Click(); // Output: Button clicked! → Handler called!
     }
 
-    static void HandleClick(object? sender, EventArgs e)
+    public class Program
     {
-        Console.WriteLine("Handler called!");
+
+        public delegate int MyDelegate(int a, int b);
+        public delegate bool Filter(int x);
+        public delegate void Callback();        
+
+        // MyDelegate method
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return (x - y);
+
+        }
+
+        // Normal method to print numbers based on a filter
+        static void PrintNumbers(List<int> list, Filter filter)
+        {
+            foreach (var num in list)
+            {
+                if (filter(num))
+                    Console.WriteLine(num);
+            }
+        }
+
+        // Callback function in Delegate
+        static void DoWork(Callback callback)
+        {
+            Console.WriteLine("Work started...");
+            callback();
+        }
+
+        static void Main()
+        {
+
+            // EventHandler example
+            OrderService order = new OrderService();
+            EmailService email = new EmailService();
+            SmsService sms = new SmsService();
+            order.OnOrderPlaced += email.SendEmail;
+            order.OnOrderPlaced += sms.SendSms;
+            order.PlaceOrder("ORD123");            
+
+
+            DoWork(() => Console.WriteLine("Work completed!"));
+
+            List<int> numbers = new List<int>();
+            PrintNumbers(numbers, x => x > 5);
+            PrintNumbers(numbers, x => x % 2 == 0);
+
+            Console.WriteLine(numbers);
+
+            Program program = new Program();
+
+            MyDelegate del = program.Add;   // store method
+            MyDelegate del1 = program.Sub;   // store method
+            int result = del(5, 3); // call method
+            int result1 = del1(5, 3); // call method
+
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+
+            // Hashcode - unique identifier for an object
+            string name = "Kumar";
+            int hashCode = name.GetHashCode(); // → oru unique number kedaikum
+            Console.WriteLine(hashCode); // e.g: 1823746
+
+
+            Calculator calc = new Calculator();
+            int calculateResult = calc.Add(10, 5);
+            Console.WriteLine(calculateResult);
+
+            // Linq
+            List<int> numerics = new List<int>() { 1, 2, 3, 4, 5 };
+
+            var resultLinq = numbers.Where(x => x > 3).ToList();
+            Console.WriteLine(string.Join(", ", resultLinq));
+
+
+            // Event handling
+            Button btn = new Button();
+
+            // Subscribe to the event
+            btn.OnClick += HandleClick;
+
+            btn.Click(); // Output: Button clicked! → Handler called!
+        }
+
+        static void HandleClick(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Handler called!");
+        }
     }
 }
